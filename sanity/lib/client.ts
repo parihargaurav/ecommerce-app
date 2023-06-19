61aaa83b05ad5b476985fb45783e3feb9d2fc9ea
Gaurav <parihargaurav527@gmail.com>
2023-06-19 23:01:59 +0530

    Deployment

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skJzqez3tthJNcfzV28hed8AcCpejU9V9e6u40ZMhxvzjSO22h30D2vZMfFReAQHIh6D0YcapLRDPIfA0okkoB4oV4iK56A2Baaax05AglGqhxUj6A5DGBMcsUyk8uKJbDeOKKiMCt0Q3AJ8OoulfoCjUzTujdfTQSUJinFXv1UYGzNwusQh",
   })
